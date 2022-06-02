17bd4b9975fd03301b160b24bd0467b5baf6daa2
timeworn <nikolay.voronin93@gmail.com>
2022-06-02 20:26:35 +0800

    fix: fixed the ViewPropTypes in native running.

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import MainStackNavigator from "./src/navigation/MainStackNavigator";
 import {MagicContextProvider} from "./src/providers/MagicContext";
 import {AuthContextProvider} from "./src/providers/AuthContext";
+import {LogBox} from "react-native";
+
+LogBox.ignoreLogs([
+    "ViewPropTypes will be removed",
+    "ColorPropType will be removed",
+])
 
 export default function App() {
     return (
